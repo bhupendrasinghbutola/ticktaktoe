805005db334af63d542e148d11de013aee5b69cf
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-05-27 00:59:14 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App=() =>{
+return (
+<React.Fragment>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this </p>
+    <small> this is me bhuppi.</small>
+  </React.Fragment>
+);
+};
+
+
+
+export default App;
