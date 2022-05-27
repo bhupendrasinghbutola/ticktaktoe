3e0af44dd2d9da3d93d79931ee84b2f9b70f4327
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-05-27 13:41:43 +0530

    styles scss added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import Square from '../Square';
+import Square from './Square';
+// import "./Square.style.css";
 
 const Board = () => {
   return (
-    <div>
+    <div className='board'>
        
-<div>
+<div className='board-row'>
 <Square  value={1} / >  
 <Square value={2}/>
 <Square value={3}/>
+{/* <Square><p>hllo</p></Square> */}
+
 </div>
-<div>
+<div className='board-row'>
 <Square value={0} />
 <Square value={5}/>
 <Square value={6}/>
 </div>
-<div>
+<div className='board-row'>
        <Square value={7}/>
        <Square value={8}/>
        <Square value={9}/>
