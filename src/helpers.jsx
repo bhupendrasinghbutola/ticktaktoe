7f8d5a4d354765e4668e5a807d64202b6947586a
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-05-27 23:36:13 +0530

    transform state to game history

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -11,6 +11,7 @@
     ];
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
+      console.log(lines[i].length)
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
